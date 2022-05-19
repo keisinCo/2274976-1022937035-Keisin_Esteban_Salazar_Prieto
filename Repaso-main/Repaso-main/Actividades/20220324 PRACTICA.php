f70f990dea7b325f47c17b896c67c5b71eab0e99
keisinCo <101893995+keisinCo@users.noreply.github.com>
2022-05-19 16:25:43 -0500

    Actividades
    Actividades hasta el día 13 de mayo del 2022

---
+++ Repaso-main/Repaso-main/Actividades/20220324 PRACTICA.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+<body>
+    <h1>Practica en php</h1>
+    <?php
+    //INICIA CODIGO EN PHP
+    //Esto es un comentario
+     /*Todo lo que este aqui dentro
+     se considera un comentario
+     */
+    //COMANDO ECHO DEVUELVE UN VALOR
+    echo"Hola soy un parrafo";
+    echo "<br>";  //salto de linea
+    echo 3+3;     //realiza operaciones en html
+                  /*que no se puede en html*/
+
+    //TERMINA PHP
+
+    ?>
+    </body>
+
+
+</head>
---
+++ Repaso-main/Repaso-main/Actividades/procesar.php
@@ -0,0 +1,5 @@
+<?php
+echo "Tu nombre es " .$_POST['nombre'] "<br>";
+echo "Tu apellido es " .$_POST['apellido'];
+
+?>
